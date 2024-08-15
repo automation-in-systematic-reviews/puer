b61aa060dd40dc236765e6292dc107835a3db376
Zhaozhen Xu <zhaozhen.xu@bristol.ac.uk>
2024-08-15 16:06:00 +0100

    add functions for threshold

---
+++ inference-api/app/funcs/threshold.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+def read_thresholds(threshold_path, topic, strategy):
+    df = pd.read_csv(threshold_path)
+    topics = df['topic'].tolist()
+    key = topic.split(' ')[0]
+    if key in topics:
+        row = df.loc[(df['topic'] == key) & (df['strategy'] == strategy)]
+        threshold = row['threshold'].values[0]
+    else:
+        print('topic not found, using the default threshold = 0.5')
+        threshold = 0.5
+
+    return threshold
+
+def threshold_to_binary_labels(predicted_value, threshold=0.5, similarity=True):
+    binary_labels = []
+    if similarity:
+        smaller_than_threshold = 'excluded'
+        larger_than_threshold = 'included'
+    else:
+        smaller_than_threshold = 'excluded'
+        larger_than_threshold = 'included'
+    for v in predicted_value:
+        if v <= threshold:
+            binary_labels.append(smaller_than_threshold)
+        else:
+            binary_labels.append(larger_than_threshold)
+    
+    return binary_labels
